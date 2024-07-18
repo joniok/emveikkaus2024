84a9a72c3b6bf6dbc7e22b224d0f32e97a1ed667
jonioks <joni@oksanen.xyz>
2024-07-18 10:25:46 +0300

    Final points. Spain is the European champions 2024.

--- update_site.R
+++ update_site.R
@@ -1,3 +1,3 @@
-#rmarkdown::render('selections.Rmd', output_format = 'html_document')
+rmarkdown::render('selections.Rmd', output_format = 'html_document')
 rmarkdown::render('index.Rmd', output_format = "html_document")
 #rmarkdown::render('analysis.Rmd', output_format = 'html_document')
